886a0b34e5c363dbc918dbc988775ccaeb5242f0
rosscodingrockstars <hey@jenniferross.tech>
2020-06-23 13:59:34 -0400

    added cursor value and added js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-//const firstCol = document.getElementById("first-col");
-//const food = "watermelon";
-//firstCol.innerHTML = `<p> Testing... testing.. 123 and ${food}. </p>`;
-const numArray = [1, 2, 3, 4, 5];
-let result = 0;
-for (let i = 0; i < numArray.length; i++) {
-  let currentNumber = numArray[i];
-  result += currentNumber;
-  console.log(result);
-}
+$("#wins").text("1");
+
+$("#money1").click(function() {
+  alert("money");
+});
+$(".btn").click(function() {
+  alert("hi");
+});
