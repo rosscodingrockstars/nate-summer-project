317213b583ae5657c6f1ebc8bf7035f96a4e696b
rosscodingrockstars <hey@jenniferross.tech>
2020-06-26 14:00:58 -0400

    getting random numbers

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
-$("#wins").text("1");
+// $("#wins").text("1");
 
-$("#money1").click(function() {
-  alert("money");
-});
-$(".btn").click(function() {
-  alert("hi");
-});
+// $("#money1").click(function() {
+// alert("money");
+// });
+// $(".btn").click(function() {
+//  alert("hi");
+// });
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let chosenNumber = Math.floor(Math.random() * (max - min)) + min;
+  $("#amount-needed").text(chosenNumber);
+}
+getRandomInt(19, 120);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let money1 = Math.floor(Math.random() * (max - min)) + min;
+  $("#money1").(console.log);
+}
+getRandomInt(1, 12);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let money2 = Math.floor(Math.random() * (max - min)) + min;
+  $("#money2").(console.log);
+}
+getRandomInt(1, 12);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let money3 = Math.floor(Math.random() * (max - min)) + min;
+  $("#money3").(console.log);
+}
+getRandomInt(1, 12);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let money4 = Math.floor(Math.random() * (max - min)) + min;
+  $("#money4").(console.log);
+}
+getRandomInt(1, 12);
