d1d90b32b8e8905d5646f5dfee8b1be167349f4b
rosscodingrockstars <hey@jenniferross.tech>
2020-07-14 15:47:14 -0400

    reset function

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-// $("#wins").text("1");
-
-// $("#money1").click(function() {
-// alert("money");
-// });
-// $(".btn").click(function() {
-//  alert("hi");
-// });
 let chosenNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 $("#amount-needed").text(chosenNumber);
-let points = 0;
-$("#points ").text(points);
-function checkPoints() {
-  if (points > chosenNumber) {
-losses++;
-  } else if (points === chosenNumber) {
-wins++;  } else {
-    console.log("still playing");
-  }
-}
 let moneyOne = Math.floor(Math.random() * 12 + 2);
 let moneyTwo = Math.floor(Math.random() * 12 + 2);
 let moneyThree = Math.floor(Math.random() * 12 + 2);
 let moneyFour = Math.floor(Math.random() * 12 + 2);
+let points = 0;
+$("#points ").text(points);
+$("#restart").click(function() {
+  points = 0;
+  $("#points ").text(points);
+  chosenNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+  $("#amount-needed").text(chosenNumber);
+});
+function checkPoints() {
+  if (points > chosenNumber) {
+    losses++;
+    $("#losses").text(losses);
+  } else if (points === chosenNumber) {
+    wins++;
+    $("#wins").text(wins);
+  }
+}
 let wins = 0;
+$("#wins").text(wins);
 let losses = 0;
-
+$("#losses").text(losses);
 $("#money1").click(function() {
   points += moneyOne;
-  $("#points ").text(points);
+  $("#points").text(points);
   checkPoints();
 });
 
